fc300b6ab852a17a97a898ba2f495cea2c791f6a
lorenaborges256 <lorena.borges.amaral@gmail.com>
2026-02-13 19:27:45 +1100

    feat!: get Random Pokemon from npm package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-const { getPokemonName } = require("./getPokemonName"); //require things from anotherfile and store in a variable
+// const { getPokemonName } = require("./getPokemonName"); //require things from anotherfile and store in a variable
+
+// Methode 1 for importing and using stuff from packages
+// const pokemon = require("pokemon"); //require the pokemon package and store in a variable
+// pokemon.random(); //use the random function from the pokemon package
+
+
+// Methode 2 for importing and using stuff from packages
+// This is object destructuring, we are taking the random function from the pokemon package and storing it in a variable called random
+// const {random} = require("pokemon"); 
+// Alias is preferred when importing generics things
+const {random: pokemonRandom} = require("pokemon"); 
+console.log(pokemonRandom()); //use the random function from the pokemon package
 
 console.log("Hello, World!");
 
+// console.log(pokemon.random()); 
 
 // Import a function and run it.
 
-async function app(){
-    let pokemonName = await getPokemonName();
-    console.log(getPokemonName);
-    console.log(await getPokemonName()); 
-    console.log(pokemonName); 
-}
+// async function app(){
+//     let pokemonName = await getPokemonName();
+//     console.log(getPokemonName);
+//     console.log(await getPokemonName()); 
+//     console.log(pokemonName); 
+// }
 
-app();
+// app();
+
